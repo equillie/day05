eab94aec6913f8938c4c865100e71613043763dd
Eric QUILLIER <equillie@e2r9p14.42.fr>
2018-07-18 18:03:44 +0200

    Correction de l'exo 2

--- ex02/ft_atoi.c
+++ ex02/ft_atoi.c
@@ -6,40 +6,33 @@
 /*   By: equillie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/09 16:31:57 by equillie          #+#    #+#             */
-/*   Updated: 2018/07/10 15:42:12 by equillie         ###   ########.fr       */
+/*   Updated: 2018/07/18 17:48:39 by equillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int		ft_strlen(char *str)
-{
-	int		i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
 
 int		ft_atoi(char *str)
 {
 	int		i;
 	int		nb;
-	int		len;
+	int		signe;
 
 	i = 0;
 	nb = 0;
-	len = ft_strlen(str) - 1;
-	if (str[0] == '-' || str[0] == '+')
+	signe = 1;
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\r' || str[i] == '\v' || str[i] == '\f' || str[i] == ' ')
 		i++;
-	while (len >= 0 && str[i] >= '0' && str[i] <= '9')
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			signe = 0;
+		i++;
+	}
+	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
 	{
 		nb = nb * 10 + (str[i] - '0');
 		i++;
-		len--;
 	}
-	if (str[0] == '-')
+	if (signe == 0)
 		nb = -nb;
 	return (nb);
 }
